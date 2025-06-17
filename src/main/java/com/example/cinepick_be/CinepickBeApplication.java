d0b5684de6dd173873429f89e3667f8b603b4463
seyoung <20200927@sungshin.ac.kr>
2025-06-18 01:11:00 +0900

    Initial Commit

---
+++ src/main/java/com/example/cinepick_be/CinepickBeApplication.java
@@ -0,0 +1,16 @@
+package com.example.cinepick_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CinepickBeApplication {
+
+   public static void main(String[] args) {
+
+      SpringApplication.run(CinepickBeApplication.class, args);
+
+   }
+
+}
---
+++ src/test/java/com/example/cinepick_be/CinepickBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinepick_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinepickBeApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
